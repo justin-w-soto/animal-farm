be84e9ad54efb57999659034bad6928f3fffee04
Justin W Soto <soto@pdx.edu>
2021-06-23 16:44:44 -0700

    added images, sound, and style

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-// import functions
+//cat
+const catSounds = document.getElementById('cat-sounds');
+const catImage = document.getElementById('cat-image');
+catImage.addEventListener('click', () => {
+    catSounds.play();
+});
+
+
+
+//dog
+const dogSounds = document.getElementById('dog-sounds');
+const dogImage = document.getElementById('dog-image');
+dogImage.addEventListener('click', () => {
+    dogSounds.play();
+});
+
+
+//horse
+const horseSounds = document.getElementById("horse-sounds");
+const horseImage = document.getElementById('horse-image');
+horseImage.addEventListener('click', () => {
+    horseSounds.play();
+});
 
 // reference needed DOM elements
 
